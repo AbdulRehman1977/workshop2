fdd0d37bdeb4afe33aaa2a9a9df5d2989f4adfea
abdulrehman <instagramabdulrehman03gmail.com>
2025-10-17 11:47:34 +0200

    Changed the message printed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     name = input("Enter your name: ")
     print(f"Your name is {name}")
+    age = int(input("What is your age: "))
+    print(f"Your age is {age}")
 
 
 if __name__ == "__main__":
